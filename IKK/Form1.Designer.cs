43006663546e699775138201378c3b0e0df1ed75
Csáktornyai Ádám <jockahun@gmail.com>
2019-11-07 22:16:48 +0100

    Form size change

--- IKK/Form1.Designer.cs
+++ IKK/Form1.Designer.cs
@@ -45,7 +45,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 561);
             this.Controls.Add(this.viewContainerMain);
-            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.MinimumSize = new System.Drawing.Size(900, 500);
             this.Name = "FormMain";
             this.Text = "IKK";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
