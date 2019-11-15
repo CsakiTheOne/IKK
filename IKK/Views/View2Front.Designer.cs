f84f7a5bbbabf9edc40815199690c77d4f4b71e5
Csáktornyai Ádám <jockahun@gmail.com>
2019-11-15 21:50:14 +0100

    User search

--- IKK/Views/View2Front.Designer.cs
+++ IKK/Views/View2Front.Designer.cs
@@ -28,201 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.split = new System.Windows.Forms.SplitContainer();
-            this.card1 = new IKK_controls.Card();
-            this.lbl1 = new IKK_controls.Lbl();
-            this.lbl2 = new IKK_controls.Lbl();
-            this.card2 = new IKK_controls.Card();
-            this.lbl3 = new IKK_controls.Lbl();
-            this.tb1 = new IKK_controls.Tb();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.lbl4 = new IKK_controls.Lbl();
-            this.tb2 = new IKK_controls.Tb();
-            this.btn1 = new IKK_controls.Btn();
-            ((System.ComponentModel.ISupportInitialize)(this.split)).BeginInit();
-            this.split.Panel1.SuspendLayout();
-            this.split.Panel2.SuspendLayout();
-            this.split.SuspendLayout();
-            this.card1.SuspendLayout();
-            this.card2.SuspendLayout();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pCenter = new System.Windows.Forms.Panel();
+            this.cardSearch = new IKK_controls.Card();
+            this.lblSearch = new IKK_controls.Lbl();
+            this.tbSearch = new IKK_controls.Tb();
+            this.btnSearch = new IKK_controls.Btn();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.pCenter.SuspendLayout();
+            this.cardSearch.SuspendLayout();
             this.SuspendLayout();
             // 
-            // split
+            // tableLayoutPanel1
             // 
-            this.split.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.split.Location = new System.Drawing.Point(8, 8);
-            this.split.Margin = new System.Windows.Forms.Padding(0);
-            this.split.Name = "split";
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.pCenter, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(900, 500);
+            this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // split.Panel1
+            // pCenter
             // 
-            this.split.Panel1.Controls.Add(this.card1);
-            this.split.Panel1MinSize = 300;
+            this.pCenter.Controls.Add(this.cardSearch);
+            this.pCenter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pCenter.Location = new System.Drawing.Point(180, 0);
+            this.pCenter.Margin = new System.Windows.Forms.Padding(0);
+            this.pCenter.Name = "pCenter";
+            this.pCenter.Padding = new System.Windows.Forms.Padding(8);
+            this.pCenter.Size = new System.Drawing.Size(540, 500);
+            this.pCenter.TabIndex = 0;
             // 
-            // split.Panel2
+            // cardSearch
             // 
-            this.split.Panel2.Controls.Add(this.card2);
-            this.split.Panel2MinSize = 300;
-            this.split.Size = new System.Drawing.Size(884, 484);
-            this.split.SplitterDistance = 300;
-            this.split.TabIndex = 0;
-            // 
-            // card1
-            // 
-            this.card1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.cardSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.card1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.card1.Controls.Add(this.lbl2);
-            this.card1.Controls.Add(this.lbl1);
-            this.card1.Location = new System.Drawing.Point(8, 8);
-            this.card1.Margin = new System.Windows.Forms.Padding(8);
-            this.card1.Name = "card1";
-            this.card1.Padding = new System.Windows.Forms.Padding(8);
-            this.card1.Size = new System.Drawing.Size(284, 100);
-            this.card1.TabIndex = 0;
+            this.cardSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardSearch.Controls.Add(this.btnSearch);
+            this.cardSearch.Controls.Add(this.tbSearch);
+            this.cardSearch.Controls.Add(this.lblSearch);
+            this.cardSearch.Location = new System.Drawing.Point(16, 16);
+            this.cardSearch.Margin = new System.Windows.Forms.Padding(8);
+            this.cardSearch.Name = "cardSearch";
+            this.cardSearch.Padding = new System.Windows.Forms.Padding(8);
+            this.cardSearch.Size = new System.Drawing.Size(508, 60);
+            this.cardSearch.TabIndex = 0;
             // 
-            // lbl1
+            // lblSearch
             // 
-            this.lbl1.AutoSize = true;
-            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lbl1.Location = new System.Drawing.Point(16, 16);
-            this.lbl1.Margin = new System.Windows.Forms.Padding(8, 8, 0, 0);
-            this.lbl1.Name = "lbl1";
-            this.lbl1.Size = new System.Drawing.Size(91, 20);
-            this.lbl1.TabIndex = 0;
-            this.lbl1.Text = "Mai idézet";
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSearch.Location = new System.Drawing.Point(16, 18);
+            this.lblSearch.Margin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(198, 20);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Felhasználók keresése:";
             // 
-            // lbl2
+            // tbSearch
             // 
-            this.lbl2.AutoSize = true;
-            this.lbl2.Location = new System.Drawing.Point(16, 44);
-            this.lbl2.Margin = new System.Windows.Forms.Padding(8, 8, 0, 0);
-            this.lbl2.Name = "lbl2";
-            this.lbl2.Size = new System.Drawing.Size(16, 13);
-            this.lbl2.TabIndex = 1;
-            this.lbl2.Text = "...";
-            // 
-            // card2
-            // 
-            this.card2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.tbSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.card2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.card2.Controls.Add(this.btn1);
-            this.card2.Controls.Add(this.tb2);
-            this.card2.Controls.Add(this.lbl4);
-            this.card2.Controls.Add(this.checkBox1);
-            this.card2.Controls.Add(this.tb1);
-            this.card2.Controls.Add(this.lbl3);
-            this.card2.Location = new System.Drawing.Point(8, 8);
-            this.card2.Margin = new System.Windows.Forms.Padding(8);
-            this.card2.Name = "card2";
-            this.card2.Padding = new System.Windows.Forms.Padding(8);
-            this.card2.Size = new System.Drawing.Size(564, 140);
-            this.card2.TabIndex = 0;
+            this.tbSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbSearch.Location = new System.Drawing.Point(222, 16);
+            this.tbSearch.Margin = new System.Windows.Forms.Padding(8);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Secret = false;
+            this.tbSearch.Size = new System.Drawing.Size(152, 26);
+            this.tbSearch.TabIndex = 1;
             // 
-            // lbl3
+            // btnSearch
             // 
-            this.lbl3.AutoSize = true;
-            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lbl3.Location = new System.Drawing.Point(16, 16);
-            this.lbl3.Margin = new System.Windows.Forms.Padding(8, 8, 0, 0);
-            this.lbl3.Name = "lbl3";
-            this.lbl3.Size = new System.Drawing.Size(186, 20);
-            this.lbl3.TabIndex = 1;
-            this.lbl3.Text = "Napi idézet beküldése";
+            this.btnSearch.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(44)))), ((int)(((byte)(0)))));
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSearch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnSearch.Location = new System.Drawing.Point(390, 14);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(8);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Raised = true;
+            this.btnSearch.Size = new System.Drawing.Size(100, 30);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Keresés";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
-            // tb1
-            // 
-            this.tb1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tb1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tb1.Location = new System.Drawing.Point(16, 44);
-            this.tb1.Margin = new System.Windows.Forms.Padding(8);
-            this.tb1.Multiline = true;
-            this.tb1.Name = "tb1";
-            this.tb1.Secret = false;
-            this.tb1.Size = new System.Drawing.Size(530, 40);
-            this.tb1.TabIndex = 2;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(20, 107);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(15, 14);
-            this.checkBox1.TabIndex = 3;
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // lbl4
-            // 
-            this.lbl4.AutoSize = true;
-            this.lbl4.Location = new System.Drawing.Point(46, 107);
-            this.lbl4.Margin = new System.Windows.Forms.Padding(8, 8, 0, 0);
-            this.lbl4.Name = "lbl4";
-            this.lbl4.Size = new System.Drawing.Size(62, 13);
-            this.lbl4.TabIndex = 4;
-            this.lbl4.Text = "Más alkotó:";
-            // 
-            // tb2
-            // 
-            this.tb2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tb2.Location = new System.Drawing.Point(116, 100);
-            this.tb2.Margin = new System.Windows.Forms.Padding(8);
-            this.tb2.Name = "tb2";
-            this.tb2.ReadOnly = true;
-            this.tb2.Secret = false;
-            this.tb2.Size = new System.Drawing.Size(150, 26);
-            this.tb2.TabIndex = 5;
-            // 
-            // btn1
-            // 
-            this.btn1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btn1.FlatAppearance.BorderSize = 0;
-            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btn1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.btn1.Location = new System.Drawing.Point(446, 98);
-            this.btn1.Margin = new System.Windows.Forms.Padding(8);
-            this.btn1.Name = "btn1";
-            this.btn1.Raised = true;
-            this.btn1.Size = new System.Drawing.Size(100, 30);
-            this.btn1.TabIndex = 6;
-            this.btn1.Text = "Beküldés";
-            this.btn1.UseVisualStyleBackColor = false;
-            // 
-            // ViewFront
+            // View2Front
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.split);
-            this.Name = "ViewFront";
-            this.split.Panel1.ResumeLayout(false);
-            this.split.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.split)).EndInit();
-            this.split.ResumeLayout(false);
-            this.card1.ResumeLayout(false);
-            this.card1.PerformLayout();
-            this.card2.ResumeLayout(false);
-            this.card2.PerformLayout();
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "View2Front";
+            this.Padding = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.pCenter.ResumeLayout(false);
+            this.cardSearch.ResumeLayout(false);
+            this.cardSearch.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer split;
-        private IKK_controls.Card card1;
-        private IKK_controls.Lbl lbl2;
-        private IKK_controls.Lbl lbl1;
-        private IKK_controls.Card card2;
-        private IKK_controls.Lbl lbl3;
-        private IKK_controls.Tb tb1;
-        private IKK_controls.Btn btn1;
-        private IKK_controls.Tb tb2;
-        private IKK_controls.Lbl lbl4;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel pCenter;
+        private IKK_controls.Card cardSearch;
+        private IKK_controls.Lbl lblSearch;
+        private IKK_controls.Tb tbSearch;
+        private IKK_controls.Btn btnSearch;
     }
 }
--- IKK/Views/View2Front.cs
+++ IKK/Views/View2Front.cs
@@ -16,5 +16,17 @@ namespace IKK
         {
             InitializeComponent();
         }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (Storage.OfflineMode) IKK_controls.MsgBox.Show("Offline mód", "Ez a funkció nem működik offline módban.");
+
+            DataTable response = IKK_data.Database.GetData($"SELECT id, email, name, about FROM user WHERE name LIKE '%{tbSearch.Text}%';");
+            if (response.Rows.Count < 1) return;
+            object[] cols = response.Rows[0].ItemArray;
+            string answer = $"({cols[0]}) {cols[1]}: {cols[2]}\n\r{cols[3]}";
+            IKK_controls.MsgBox.Show(tbSearch.Text, answer);
+            tbSearch.Clear();
+        }
     }
 }
