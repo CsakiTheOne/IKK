44e42fc888c5ae6b9d6e7f8b246f928c44126834
Csáktornyai Ádám <jockahun@gmail.com>
2019-12-08 20:09:17 +0100

    Added settings

---
+++ IKK database/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKK_data
+{
+    public static class Settings
+    {
+        public static void Set(string key, object value)
+        {
+            Properties.Settings.Default[key] = value;
+            Properties.Settings.Default.Save();
+        }
+
+        public static T Get<T>(string key, T defaultValue)
+        {
+            if (Properties.Settings.Default[key] == null) return defaultValue;
+            return (T)Properties.Settings.Default[key];
+        }
+
+        /// <summary>
+        /// This method is not safe to use and can delete data.
+        /// </summary>
+        public static void Clear()
+        {
+            Properties.Settings.Default.Reset();
+        }
+    }
+}
